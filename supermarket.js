24708f150eea875feeb7207360a6c083fdc5b23d
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-05-01 18:19:22 +1000

    solution

--- supermarket.js
+++ supermarket.js
@@ -1,32 +1,49 @@
 class Till {
-    constructor() {
-        this.customerTime = [];
-    }
-    totalTime() {
-        // return sum of time required by all the customerTime
-        // return 0 if array is empty
-    }
-    addCustomer(time) {
-        this.customerTime.push(time);
-    }
+	constructor(id) {
+    this.id = id;
+		this.customerTime = [];
+	}
+	totalTime() {
+		return this.customerTime.reduce((a, b) => a + b, 0)
+	}
+	addCustomer(time) {
+		this.customerTime.push(time);
+	}
 }
 
 function tillWithSmallestQueue(tills) {
-    // return till with lowest totalTime
+    let s =  tills.reduce((t1, t2) => {
+      if (t1.totalTime() <= t2.totalTime()) {
+        return t1;
+      } else {
+        return t2;
+      }
+    }, tills[0]);
+
+  return s;
 }
 
 function tillWithLargestQueue(tills) {
-    // return till with highest totalTime
+    let l = tills.reduce((t1, t2) => {
+        if(t1.totalTime() > t2.totalTime()) {
+          return t1;
+        } else {
+          return t2;
+        }
+    }, tills[0]);  
+    return l;
 }
 
 function queueTime(customers, n) {
-    // Your code here
-    tills = Array(n).map(t => new Till());
-
+    let tills = [];
+    for(let i=0; i<n; i++) {
+      tills.push(new Till(i+1))
+    }
     // process each customer
     customers.forEach(customer => {
         // get till with lowest totalTime
         let smallestTill = tillWithSmallestQueue(tills);
+        
         // add customer to that till
         smallestTill.addCustomer(customer);
     });
@@ -36,4 +53,5 @@ function queueTime(customers, n) {
     // that's the output we need
     return largestTill.totalTime();
 }
+
 module.exports = {queueTime}
